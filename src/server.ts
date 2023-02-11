dae68511d335743ed2c7394f943ea1e26118779b
c0ffex <yudisakuma@gmail.com>
2023-02-11 01:13:34 -0300

    feat: courses table created and added to adminJS

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express'
+import { adminJs, adminJsRouter } from './adminjs';
 import { sequelize } from './database';
 require('dotenv').config();
+
 const app = express()
+
+app.use(express.static('public'))
+
+app.use(adminJs.options.rootPath, adminJsRouter)
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   sequelize.authenticate().then(() => {
-    console.log('hi')
+    console.log('DB connection established')
   })
   console.log(`Server started succesfully at port ${PORT}`)
 })
